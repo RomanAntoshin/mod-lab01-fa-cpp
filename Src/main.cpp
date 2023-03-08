b0bce4c547259c1f345dfc349179da6f11b0915c
RomanA <126502093+RomanAntoshin@users.noreply.github.com>
2023-03-08 15:22:15 +0300

    Update and rename src/main.cpp to Src/main.cpp

---
+++ Src/main.cpp
@@ -0,0 +1,10 @@
+// Copyright 2022 UNN-IASR
+#include "fun.h"
+
+int main() {
+    char str[] = {"L2 U Error st4 ehte"};
+    cout << str<<"\n";
+    cout << faStr1(str)<<"\n";
+    cout << faStr2(str)<<"\n";
+    cout << faStr3(str) << "\n";
+}
--- src/main.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright 2022 UNN-IASR
-#include "fun.h"
-
-int main() {
-    return 0;
-}
