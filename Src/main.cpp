06948e23c073a90fa538e77a26ad5d76db488476
RomanA <126502093+RomanAntoshin@users.noreply.github.com>
2023-03-08 17:06:49 +0300

    Update main.cpp

--- Src/main.cpp
+++ Src/main.cpp
@@ -3,8 +3,8 @@
 
 int main() {
     char str[] = {"L2 U Error st4 ehte"};
-    cout << str<<"\n";
-    cout << faStr1(str)<<"\n";
-    cout << faStr2(str)<<"\n";
+    cout << str << "\n";
+    cout << faStr1(str) << "\n";
+    cout << faStr2(str) << "\n";
     cout << faStr3(str) << "\n";
 }
