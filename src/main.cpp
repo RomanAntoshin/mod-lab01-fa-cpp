282daff4279a3088b7c5e799631368bd0570fb44
RomanA <126502093+RomanAntoshin@users.noreply.github.com>
2023-03-08 22:16:11 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,8 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
-using namespace std;
 
 int main() {
-    char str[] = {"L2 U Error st4 ehte"};
-    cout << str << "\n";
-    cout << faStr1(str) << "\n";
-    cout << faStr2(str) << "\n";
-    cout << faStr3(str) << "\n";
+    faStr1(str);
+    faStr2(str);
+    faStr3(str);
 }
